342c18f0e60a593a9265d284861a6752bea6986b
Zara1365 <114822777+Zara1365@users.noreply.github.com>
2022-12-16 21:26:30 +0200

    Add files via upload

---
+++ CarTask/Car.java
@@ -0,0 +1,34 @@
+package CarTask;
+
+public class Car{
+  
+     public String brand ;
+     public String model ;
+     public int amountOfFuel;
+
+public Car(String brand,String model,int amountOfFuel){
+         this.brand = brand;
+         this.model = model;
+         this.amountOfFuel = amountOfFuel;
+     }
+     
+     public Car(){
+         this.brand = "";
+         this.model = "";
+         this.amountOfFuel = 0;
+     }
+    public void brake(){
+        System.out.println("Car is breaking");
+    }
+    public void accelerate(){
+        if(this.amountOfFuel > 0){
+            amountOfFuel--;
+            System.out.println("Car is accelerating");
+        }
+    }
+     public void printData(){
+         System.out.println("Brand: " + this.brand);
+         System.out.println("Model: " + this.model);
+         System.out.println("Fuel: " + this.amountOfFuel);
+     }
+}
